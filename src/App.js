c45ca8af5868be6fbd09eff0b1b9d82933627b9b
Jack Gay <jackpowis@gmail.com>
2020-04-30 22:16:11 +0100

    commit: component layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Link } from "react-router-dom";
+import HomePage from './components/HomePage/HomePage'
+import AboutPage from './components/AboutPage/AboutPage'
+import SubmitContact from './components/SubmitContact/SubmitContact'
 
 
 
@@ -14,10 +17,9 @@ class App extends Component {
           <Link>Contact Us</Link>
         </nav>
         <main>
-          <Route exact path="/" />
-          <Route path="/about-us" />
-          <Route path ="/contact-us" />
-
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about-us" component={AboutPage} />
+          <Route path="/contact-us" component={SubmitContact} />
 
         </main>
         <footer>
