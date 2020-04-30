32e1956ae0a2e40d75d5fd7afeb5147d5b7251ff
Jack Gay <jackpowis@gmail.com>
2020-04-30 22:05:15 +0100

    commit 4: added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link>Home</Link>
+          <Link>About Us</Link>
+          <Link>Contact Us</Link>
+        </nav>
+        <main>
+          <Route exact path="/" />
+          <Route path="/about-us" />
+          <Route path ="/contact-us" />
+
+
+        </main>
+        <footer>
+
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
